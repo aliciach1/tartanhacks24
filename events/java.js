e6c270feb879cf9252b5ee771b1efea188867527
Opally <Kanyopally15@gmail.com>
2024-02-03 00:36:18 -0500

    more add yay

--- events/java.js
+++ events/java.js
@@ -4,3 +4,22 @@ var dates = document.querySelectorAll("section time.hidden");
         setTimeout(function(){ caldate.classList.remove("hidden") }, 250*i)
         i++;
 })
+
+var img_count = 0;
+//Get img count idk hehe
+if (img_count == 1) {
+    //get date from the img
+    //add img to html and add class accordingly
+} else if (img_count == 2) {
+    //get date from the img
+    //add img to html and add class accordingly
+} else if (img_count == 3) {
+    //get date from the img
+    //add img to html and add class accordingly
+} else if (img_count == 4) {
+    //get date from the img
+    //add img to html and add class accordingly
+} else {
+    //get date from the img
+    //add img to html and add class accordingly
+}
